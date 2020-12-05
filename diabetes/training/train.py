3339394852e589f812d5e767e0fc8ed9d7713aa9
Andy Song <congxisong@hotmail.com>
2020-12-05 15:11:22 +0000

    train.py finished

--- diabetes/training/train.py
+++ diabetes/training/train.py
@@ -1,9 +1,13 @@
-from azureml.core import Datastore,Workspace,Dataset
+from azureml.core import Datastore,Dataset
 from azureml.core.run import Run
+from sklearn.model_selection import train_test_split
+from skelearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
 import argparse
 import pprint
 import json
 import os
+import joblib
 
 def register_dataset(workspace,
                      datastore_name,
@@ -17,6 +21,24 @@ def register_dataset(workspace,
                                create_new_version=True)#either create new version if existed of exist_ok=True
     
     return dataset
+
+def split_data(df):
+    X = df.drop('Y',axis=1).values
+    Y = df['Y']
+    x_train,x_test,y_train,y_test = train_test_split(X,Y,test_size=0.2,random_state=5)
+    return {'train':{'X':x_train,'Y':y_train},
+            'test':{'X':x_test,'Y':y_test}}
+
+def train_model(data,ridge_args):
+    ridge_model = Ridge(**ridge_args)
+    ridge_model.fit(data['train']['X'],data['train']['Y'])
+    return ridge_model
+
+def get_model_metrics(model,data):
+    preds = model.predict(data['test']['X'])
+    mse = mean_squared_error(data['test']['Y'],preds)
+    metric = {'mse':mse}
+    return metric
 
 def main():
     parser = argparse.ArgumentParser()
@@ -71,25 +93,28 @@ def main():
     else:
         raise Exception('No dataset is provided')
     
-    run.input_datasets['training'] = dataset
-
-#%%
-
+    run.input_datasets['training_data'] = dataset
+    run.parent.tag('dataset_id',dataset.id)
     
-#parser = argparse.ArgumentParser()
-#arg = parser.add_argument
-#arg('--model-name',type=str,default='diabetes_model.pkl',
-#    help='the name of the model')
-#arg('--dataset-name',type=str,
-#     help='dataset_name is always required to get the exact version \
-#     of dataset wanted instead of the one during pipeline creation')
-#arg('--step-output',type=str,
-#    help='output of data passing to next step')
-#arg('--dataset-version',type=str,
-#    help='wanted dataset version')
-#arg('--dataset-file-path',type=str,
-#    help='new dataset to register')
-#args = parser.parse_args()
-#print(args.dataset_file_path)
+    df = dataset.to_pandas_dataframe()
+    data = split_data(df)
+    model = train_model(df,train_args)
+    metrics = get_model_metrics(model,data)
+    for key,value in metrics.items():
+        run.log(key,value)
+        run.parent.log(key,value)
+    
+    os.mkdir(step_output,exist_ok=True)
+    model_path = os.path.join(step_output,model_name)
+    joblib.dump(value=model,filename=model_path)
+    
+    os.mkdir('outputs',exist_ok=True)
+    output_path = os.path.join('outputs',model_name)
+    joblib.dump(value=model,filename=output_path)
+    
+    run.tag('run_type',value='train')
+    run.complete()
 
-#print(arg_dict)
+if __name__ == '__main__':
+    main()
+
