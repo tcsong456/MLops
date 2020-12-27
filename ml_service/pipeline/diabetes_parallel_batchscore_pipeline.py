6433431f6f41baf1edde122874754ff92114a999
Andy Song <congxisong@hotmail.com>
2020-12-27 00:14:46 +0000

    batchscore 55

--- ml_service/pipeline/diabetes_parallel_batchscore_pipeline.py
+++ ml_service/pipeline/diabetes_parallel_batchscore_pipeline.py
@@ -18,7 +18,7 @@ def get_or_create_datastore(datastore_name,
     container_name = env.scoring_datastore_input_container if input else env.scoring_datastore_output_container
     if datastore_name in workspace.datastores:
         datastore = workspace.datastores[datastore_name]
-    elif container_name is not None and env.scoring_datastore_storage_name is not None:
+    elif container_name is not None and env.scoring_datastore_access_key is not None:
         datastore = Datastore.register_azure_blob_container(workspace=workspace,
                                                             datastore_name=datastore_name,
                                                             container_name=container_name,
@@ -50,7 +50,7 @@ def get_fallback_input_dataset(workspace,
     if datastore is None:
         datastore = workspace.get_default_datastore()
     scoring_input_ds = datastore.upload_files(files=[env.scoring_datastore_input_filename],
-                                              target_path='diabetes_scoring_input',
+                                              target_path='scoring_input',
                                               overwrite=True)
     dataset = Dataset.Tabular.from_delimited_files(scoring_input_ds).register(workspace=workspace,
                                                                               name=env.scoring_dataset_name,
